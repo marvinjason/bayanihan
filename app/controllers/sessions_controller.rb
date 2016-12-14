5d244dc3a40c6f352bc1e3fc47fe9d5c99b42d88
marvinjason <marvinjason.sy@gmail.com>
2016-12-14 17:26:53 +0800

    Updated omniauth-twitter module

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,9 @@
 class SessionsController < ApplicationController
   def create
-  	render text: request.env['omniauth.auth'].to_yaml
+  	#render text: request.env['omniauth.auth'].to_yaml
+
+  	@user = User.from_omniauth(request.env['omniauth.auth'])
+  	session[:user_id] = @user.id
 
   	# save user to db
   	# set session['uid']
